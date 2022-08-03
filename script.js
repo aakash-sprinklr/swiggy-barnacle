1b8e7ad87e976db1ccbda9839527b67fe68f9940
Aakash <aakash.kumar@sprinklr.com>
2022-08-03 17:31:31 +0530

    issue/carousel

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const carousel = document.querySelector(".carousel");
 
 const scroll = (direction) => {
   let width =
-    Math.floor(document.querySelector(".carousel-item").scrollWidth) + 16;
+    Math.floor(document.querySelector(".carousel-item").scrollWidth) + 32;
   width = direction == "left" ? -width : width;
+  console.log(width);
 
   carousel.scrollBy({
     left: width,
